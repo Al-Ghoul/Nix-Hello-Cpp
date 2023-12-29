34fea46332adc357f2c3542fdce0530994f48a4c
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-29 17:31:11 +0200

    Update

--- release.nix
+++ release.nix
@@ -1,6 +1,21 @@
-let
-  pkgs = import <nixpkgs> { };
+{
+  helloSrc ? builtins.fetchTarball https://github.com/al-ghoul/Nix-Hello-Cpp/archive/main.tar.gz
+}:
+let 
+  pkgs = (import <nixpkgs> { system = builtins.currentSystem or "x86_64-linux"; });
+  jobs = rec {
+    build = pkgs.stdenv.mkDerivation { 
+      name = "Cpp-Hello";
+
+      src = helloSrc; 
+
+      buildInputs = [ pkgs.clang ] ;
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp hello $out/bin
+        '';
+    };
+  };
 in
-  {
-    hello = pkgs.callPackage ./hello.nix { };
-  }
+  jobs
